e17946d68ce521520144b82c868484b7907527fd
ahmad mehtab <ahmedmehtab@vizzwebsolutions.com>
2023-12-15 23:05:35 +0500

    header change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,9 +9,9 @@ function Header() {
     <header className="bg-slate-200 shadow-md h-20 ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sahand</span>
-            <span className="text-slate-700">Estate</span>
+          <h1 className="font-bold text-sm sm:text-2xl flex flex-wrap gap-2">
+            <span className="text-slate-500 font-serif">Khpal</span>
+            <span className="text-slate-700 font-serif">Kor</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
